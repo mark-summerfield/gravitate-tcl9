b0d16ebf42bbb4fa34d79e8ab75f43a5d5983d89
mark-summerfield <mark@qtrac.eu>
2025-07-02 12:19:16 +0100

    m

--- gravitate.tcl
+++ gravitate.tcl
@@ -1,7 +1,11 @@
 #!/usr/bin/env wish9
 # Copyright © 2020-25 Mark Summerfield. All rights reserved.
 
-const APPPATH [file normalize [file dirname [info script]]]
+if {![catch {file readlink [info script]} name]} {
+    const APPPATH [file dirname $name]
+} else {
+    const APPPATH [file normalize [file dirname [info script]]]
+}
 tcl::tm::path add $APPPATH
 
 package require app
